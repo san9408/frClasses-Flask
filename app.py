7781417860d58a27db76132651b3ad50d3e2aca7
Santiago CARDENAS <santiago.cardenas@amadeus.com>
2025-02-24 11:28:20 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, render_template, request, send_from_directory, redirect, url_for, session
+import requests
+import json
+import datetime
+import pytz
+
+time_zone = pytz.timezone("Europe/Paris") #America/Bogota
+date_time_zone = datetime.datetime.now(time_zone)
+date_formated = f'{date_time_zone.strftime('%Y-%m-%dT%H:%M:%S')}'
+date_beauty = f'{date_time_zone.strftime('%a, %d %b %Y %H:%M')}'
+
+url_get_token = 'https://back-spc.azurewebsites.net/api/v1/auth/login'
+url_get_class = f'https://back-spc.azurewebsites.net/api/v1/class?start_date={date_formated}&type=REGULAR'
+url_get_reserved_class = f'https://back-spc.azurewebsites.net/api/v1/class?start_date={date_formated}&type=REGISTERED'
+#Surl_get_class_details = f"https://back-spc.azurewebsites.net/api/v1/class/{class_id}/session"
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_index():
+    return render_template("index.html")
+
+@app.route("/log-in", methods=["POST"])
+def log_in():
+    if request.is_json:
+        data = request.get_json()
+        userId = data.get("userId")
+        token = log_in(userId)
+        classes = get_classes(token, url_get_class)
+        print(classes)
+        return jsonify(classes)
+    else:
+        return jsonify({"error": "Formato no válido, usa JSON"}), 415
+
+def log_in(user):
+    url = url_get_token
+    body = {"customer_id":f"FR{user}","password":f"{user}"}
+    x = requests.post(url, json = body)
+    response = json.loads(x.text)
+    token = response['token']
+    return token
+
+def get_classes(token, url):
+    classes = {'available':[], 'reserved':[]}
+    class_url = url
+    get_class = requests.get(class_url, headers={'Authorization': 'Bearer ' + token})
+    response_class = json.loads(get_class.text)
+    get_class_reserved = requests.get(url_get_reserved_class, headers={'Authorization': 'Bearer ' + token})
+    response_class_reserved = json.loads(get_class_reserved.text)
+    classes['available'] = response_class
+    classes['reserved'] = response_class_reserved
+    print(classes)
+    return classes
---
+++ static/interaction.js
@@ -0,0 +1,270 @@
+
+let app = document.querySelector('#app')
+
+window.getInputValue = function(event){
+    event.preventDefault()
+    let inputValue = document.getElementById("input-id").value
+
+    if (inputValue!=''){
+        logIn(inputValue)
+    } else {
+        window.alert('El campo de id esta vacio')
+    }
+    
+}
+
+function formatDate(dateISO) {
+
+    let myDate = new Date(dateISO);
+
+    let optionsDate = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', timeZone: 'America/Bogota' };
+    let optionsHour = { hour: '2-digit', minute: '2-digit', hour12: true, timeZone: 'America/Bogota' };
+
+    let formatDate = new Intl.DateTimeFormat('es-ES', optionsDate);
+    let formatHour = new Intl.DateTimeFormat('es-ES', optionsHour);
+
+    let formatedDate = formatDate.format(myDate);
+    let formatedHour = formatHour.format(myDate);
+
+    return `El ${formatedDate} a las ${formatedHour} hora Colombia`;
+}
+
+let dataClasses = {}
+
+function logIn(userId){
+
+    fetch('/log-in', {
+        method: 'POST', 
+        headers: { 
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({'userId': userId})})
+    .then(response => response.json())
+    .then(data => {
+
+        dataClasses = data
+
+        if (dataClasses.available.length > 0){
+            //Llamar funcion para crear container principales
+            let container = createContainersAndFilters('avail')
+            //Llamar funcion createCards() enviando data para iterar
+            createCards(dataClasses.available, container, 'avail')
+            addFilterEventListeners()
+
+        } else if (dataClasses.reserved.length > 0){
+
+            //Llamar funcion para crear container principales
+            let container = createContainersAndFilters('reserved')
+            //Llamar funcion createCards() enviando data para iterar
+            createCards(dataClasses.reserved, container, 'reserved')
+            addFilterEventListeners()
+
+            } else {
+            window.alert("No hay clases disponibles")
+        }
+
+    })
+}
+
+function addFilterEventListeners(){
+
+    const availRadioButtons = document.getElementById('available');
+    availRadioButtons.addEventListener("click", () => {
+
+        removeMainContainer()
+        let containerR = createContainersAndFilters('avail')
+        createCards(dataClasses.available, containerR, 'avail')
+
+    })
+
+    const reservedRadioButtons = document.getElementById('reserved');
+    reservedRadioButtons.addEventListener("click", () => {
+
+        removeMainContainer()
+        let containerR = createContainersAndFilters('reserved')
+        createCards(dataClasses.reserved, containerR, 'reserved')
+
+})
+
+}
+
+
+function createContainersAndFilters(type){
+
+    let filtersExist = document.body.contains(document.getElementById('reserved'))
+    let typeRequest = type
+    const appResults = document.getElementById('app')
+
+        // Create main container for filters
+        const containerResults = document.createElement('div')
+        containerResults.classList.add('main-container-results')
+
+        if (!filtersExist){
+            //create container filters
+            const filtersContainer = document.createElement('div')
+            filtersContainer.classList.add('radio-inputs')
+
+            //Create filter 1 "available"
+            const labelFilter1 = document.createElement('label')
+            labelFilter1.classList.add('radio')
+
+            const inputFilter1 = document.createElement('input')
+            inputFilter1.type = "radio"
+            inputFilter1.name = "radio"
+            inputFilter1.id = "available"
+            if (type==='avail') {
+                inputFilter1.checked = true
+            }
+
+            const spanFilter1 = document.createElement('span')
+            spanFilter1.classList.add('name')
+            spanFilter1.innerText = "Disponibles"
+
+            labelFilter1.append(inputFilter1, spanFilter1)
+            filtersContainer.append(labelFilter1)
+            
+            //Create filter 2 "programadas"
+
+            const labelFilter2 = document.createElement('label')
+            labelFilter2.classList.add('radio')
+
+            const inputFilter2 = document.createElement('input')
+            inputFilter2.type = "radio"
+            inputFilter2.name = "radio"
+            inputFilter2.id = "reserved"
+            if (type!='avail') {
+                inputFilter2.checked = true
+            }
+
+            const spanFilter2 = document.createElement('span')
+            spanFilter2.classList.add('name')
+            spanFilter2.innerText = "Programadas"
+
+            labelFilter2.append(inputFilter2, spanFilter2)
+            filtersContainer.append(labelFilter2)
+            
+            //agregar a app
+            appResults.append(filtersContainer)
+        }
+
+        // Create subtitle
+        const pageSubtitle = document.createElement('h2')
+        pageSubtitle.classList.add('subtitle')
+
+        if (type==='avail'){
+            pageSubtitle.innerText = "Clases disponibles"
+        } else {
+            pageSubtitle.innerText = "Clases programadas"
+        }
+        
+        containerResults.append(pageSubtitle)
+
+        //Agregar a app
+        appResults.append(containerResults)
+
+        //Llamar animacion             
+        menuAnimation(containerResults)
+
+        //Llamar funcion createCards() sending data to iterate
+        // createCards(data.available, containerResults)
+
+        return containerResults
+}
+
+
+function createCards(data, containerR, typeRequest){    
+    // Loop for creating cards
+    data.forEach((item) => {
+
+        createButton().then(svgData => {
+
+                const container = document.createElement('div')
+                container.classList.add('container')
+                
+                const cardContainer = document.createElement('div')
+                cardContainer.classList.add('card-container')
+
+                const iconCalendarContainer = document.createElement('div')
+                iconCalendarContainer.classList.add('card-icon-container')
+                iconCalendarContainer.innerHTML = svgData
+
+                if (typeRequest==='avail'){
+                    const spanButton = document.createElement('span')
+                    spanButton.classList.add('card-span-button')
+                    spanButton.innerText = 'Registrar'
+                    container.append(spanButton)
+                }
+                
+                const cardBodyContainer = document.createElement('div')
+                cardBodyContainer.classList.add('card-body-container')
+        
+                const title = document.createElement('h3')
+                title.classList.add('card-title')
+                title.innerText = item.teacher_name
+        
+                const classDate = document.createElement('p')
+                classDate.classList.add('card-body')
+                let formatedDate = formatDate(item.start_date)
+                classDate.innerText = formatedDate
+
+                cardBodyContainer.append(title, classDate)
+                cardContainer.append(iconCalendarContainer, cardBodyContainer)
+                container.append(cardContainer)
+                containerR.append(container)
+        
+                var arrowButton = document.querySelector('.arrow')
+                arrowButton.style.display = 'block'
+
+                
+                });
+
+    })
+}
+
+function menuAnimation(){
+    let w = window.innerWidth
+    let animationY = -300
+
+    if (w<500) {
+        animationY = -400
+    } 
+
+    let myAnimation = anime({
+        targets: ['.main-container', '.availabilityApp'],
+        translateY: animationY,
+        duration: 3000
+    })
+}
+
+let arrow = document.getElementById('arrow-down')
+
+arrow.addEventListener("click", (e) => {
+    menuAnimationOut()
+  });
+
+function menuAnimationOut(){
+    let myAnimation = anime({
+        targets: ['.main-container'],
+        translateY: 0,
+        duration: 3000
+    })
+    let filtersContainer = document.querySelector('.radio-inputs')
+    filtersContainer.remove()
+
+    removeMainContainer()
+
+    let arrowButton = document.querySelector('.arrow')
+    arrowButton.style.display = 'none'
+
+}
+
+async function createButton() {
+    const response = await fetch('/static/icons/calendar_icon.html')
+    const data = await response.text()
+    return data
+  }
+
+function removeMainContainer(){
+    let availabilityContainer = document.querySelector('.main-container-results')
+    availabilityContainer.remove()
+}
